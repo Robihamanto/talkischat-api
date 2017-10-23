17d18801cbf7c3ae39ade743addac93a242c0d6d
robihamanto <rrobie44@gmail.com>
2017-10-23 10:42:35 +0700

    first commit

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,9 @@
 import mongodb from 'mongodb';
 
 export default {
-  // "port": 3005,
-  // "mongoUrl": "mongodb://localhost:27017/chat-api",
-  "port": process.env.PORT,
-  "mongoUrl": process.env.MONGODB_URI,
+  "port": 3005,
+  "mongoUrl": "mongodb://localhost:27017/chat-api",
+  // "port": process.env.PORT,
+  // "mongoUrl": "mongodb://root:12345@ds129315.mlab.com:29315/talkischat",
   "bodyLimit": "100kb"
 }
